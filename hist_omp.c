9216db6b4681cbb31c71110444361a81361bba03
escolhido <marcelo.caridade@protonmail.com>
2017-12-05 22:09:35 +0000

    IDK

--- hist_omp.c
+++ hist_omp.c
@@ -17,8 +17,7 @@ int main(int argc, char** argv){
         exit(-1);
     }
 
-    omp_set_num_threads(atoi(argv[4]));
-	double start = omp_get_wtime();
+    omp_set_num_threads(atoi(argv[2]));
 
     // read from the input bmp image file
     int image_width, image_height;
@@ -29,8 +28,9 @@ int main(int argc, char** argv){
 
     // create a histogram for the pixel values
     int* histogram = (int*)calloc(sizeof(int), color_depth);
+    float* transfer_function = (float*)calloc(sizeof(float), color_depth);
 
-    double after_read = omp_get_wtime();
+    double start = omp_get_wtime();
 
 #pragma omp parallel
 {
@@ -49,10 +49,7 @@ int main(int argc, char** argv){
     }
 }
 
-    double after_histogram = omp_get_wtime();
-
     // finding the normalised values using cumulative mass function
-    float* transfer_function = (float*)calloc(sizeof(float), color_depth);
     for(int i = 0; i < color_depth; i++){
         int sum = 0;
         for(int j = 0; j <= i; j++) {
@@ -62,23 +59,15 @@ int main(int argc, char** argv){
         transfer_function[i] = ((float) color_depth * sum) / (image_size);
     }
 
-	double after_normalization = omp_get_wtime();
-
     #pragma omp parallel for
     for(int i = 0; i < image_size; i++){
         output_image[i] = transfer_function[image[i]];
     }
 
-	double after_new_image = omp_get_wtime();
+	double end = omp_get_wtime();
 
     // write data to output bmp image file
     write_bmp(output_image, image_width, image_height);
 
-    double end = omp_get_wtime();
-
     printf("Total: %f\n", end-start);
-    printf("Sem read/write: %f\n", after_new_image-after_read);
-    printf("Construção do histograma: %f\n", after_histogram-after_read);
-    printf("Normalização do histograma: %f\n", after_normalization-after_histogram);
-    printf("Construção da imagem final: %f\n", after_new_image-after_normalization);
 }
--- hist_serial.c
+++ hist_serial.c
@@ -16,8 +16,6 @@ int main(int argc, char** argv){
         exit(-1);
     }
 
-	double start = omp_get_wtime();
-
     // read from the input bmp image file
     int image_width, image_height;
     unsigned char* image = read_bmp(argv[1], &image_width, &image_height);
@@ -26,18 +24,17 @@ int main(int argc, char** argv){
     const int image_size = image_width * image_height;
     unsigned char* output_image = malloc(sizeof(unsigned char) * image_size);
 
-    double after_read = omp_get_wtime();
+    int* histogram = (int*)calloc(sizeof(int), color_depth);
+    float* transfer_function = (float*)calloc(sizeof(float), color_depth);
+
+    double start = omp_get_wtime();
 
     // create a histogram for the pixel values
-    int* histogram = (int*)calloc(sizeof(int), color_depth);
     for(int i = 0; i < image_size; i++){
         histogram[image[i]]++;
     }
 
-    double after_histogram = omp_get_wtime();
-
     // finding the normalised values using cumulative mass function
-    float* transfer_function = (float*)calloc(sizeof(float), color_depth);
     for(int i = 0; i < color_depth; i++){
         int sum = 0;
         for(int j = 0; j <= i; j++) {
@@ -47,23 +44,14 @@ int main(int argc, char** argv){
         transfer_function[i] = ((float) color_depth * sum) / (image_size);
     }
 
-	double after_normalization = omp_get_wtime();
-
     for(int i = 0; i < image_size; i++){
         output_image[i] = transfer_function[image[i]];
     }
 
-	double after_new_image = omp_get_wtime();
+	double end = omp_get_wtime();
 
     // write data to output bmp image file
     write_bmp(output_image, image_width, image_height);
 
-    double end = omp_get_wtime();
-
     printf("Total: %f\n", end-start);
-    printf("Sem read/write: %f\n", after_new_image-after_read);
-    printf("Construção do histograma: %f\n", after_histogram-after_read);
-    printf("Normalização do histograma: %f\n", after_normalization-after_histogram);
-    printf("Construção da imagem final: %f\n", after_new_image-after_normalization);
-
 }
